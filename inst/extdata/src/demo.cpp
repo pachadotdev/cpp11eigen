32d2999cad3833c14465c44db788b041b042cd25
Mauricio 'Pacha' Vargas Sepulveda <m.sepulveda@mail.utoronto.ca>
2024-08-26 12:45:58 -0400

    update pkg template

--- inst/extdata/src/demo.cpp
+++ inst/extdata/src/demo.cpp
@@ -1,29 +1,28 @@
 #include <cpp11.hpp>
 #include <cpp11eigen.hpp>
 
-using namespace Eigen;
 using namespace cpp11;
 
 [[cpp11::register]] doubles_matrix<> ols_mat(const doubles_matrix<>& y,
                                              const doubles_matrix<>& x) {
-  Mat<double> Y = as_Mat(y);
-  Mat<double> X = as_Mat(x);
+  Eigen::MatrixXd Y = as_Mat(y);
+  Eigen::MatrixXd X = as_Mat(x);
 
-  Mat<double> XtX = X.t() * X;
-  Mat<double> XtX_inv = inv(XtX);
-  Mat<double> beta = XtX_inv * X.t() * Y;
+  Eigen::MatrixXd XtX = X.t() * X;
+  Eigen::MatrixXd XtX_inv = inv(XtX);
+  Eigen::MatrixXd beta = XtX_inv * X.t() * Y;
 
   return as_doubles_matrix(beta);
 }
 
 [[cpp11::register]] doubles ols_double(const doubles_matrix<>& y,
                                        const doubles_matrix<>& x) {
-  Mat<double> Y = as_Mat(y);
-  Mat<double> X = as_Mat(x);
+  Eigen::MatrixXd Y = as_Mat(y);
+  Eigen::MatrixXd X = as_Mat(x);
 
-  Mat<double> XtX = X.t() * X;
-  Mat<double> XtX_inv = inv(XtX);
-  Mat<double> beta = XtX_inv * X.t() * Y;
+  Eigen::MatrixXd XtX = X.t() * X;
+  Eigen::MatrixXd XtX_inv = inv(XtX);
+  Eigen::MatrixXd beta = XtX_inv * X.t() * Y;
 
   return as_doubles(beta);
 }
--- inst/include/wrappers/matrices.hpp
+++ inst/include/wrappers/matrices.hpp
@@ -21,10 +21,12 @@ inline Eigen::Matrix<T, Dynamic, Dynamic> dblint_matrix_to_Matrix_(const U& x) {
   const int m = x.ncol();
 
   if (std::is_same<U, doubles_matrix<>>::value) {
-    Eigen::Map<Eigen::Matrix<T, Dynamic, Dynamic>> y(reinterpret_cast<T*>(REAL(x.data())), n, m);
+    Eigen::Map<Eigen::Matrix<T, Dynamic, Dynamic>> y(reinterpret_cast<T*>(REAL(x.data())),
+                                                     n, m);
     return y;
   } else {
-    Eigen::Map<Eigen::Matrix<T, Dynamic, Dynamic>> y(reinterpret_cast<T*>(INTEGER(x.data())), n, m);
+    Eigen::Map<Eigen::Matrix<T, Dynamic, Dynamic>> y(
+        reinterpret_cast<T*>(INTEGER(x.data())), n, m);
     return y;
   }
 }
@@ -34,10 +36,12 @@ inline Eigen::Matrix<T, Dynamic, Dynamic> dblint_to_Matrix_(const U& x) {
   const int n = x.size();
 
   if (std::is_same<U, doubles>::value) {
-    Eigen::Map<Eigen::Matrix<T, Dynamic, Dynamic>> y(reinterpret_cast<T*>(REAL(x.data())), n, 1);
+    Eigen::Map<Eigen::Matrix<T, Dynamic, Dynamic>> y(reinterpret_cast<T*>(REAL(x.data())),
+                                                     n, 1);
     return y;
   } else {
-    Eigen::Map<Eigen::Matrix<T, Dynamic, Dynamic>> y(reinterpret_cast<T*>(INTEGER(x.data())), n, 1);
+    Eigen::Map<Eigen::Matrix<T, Dynamic, Dynamic>> y(
+        reinterpret_cast<T*>(INTEGER(x.data())), n, 1);
     return y;
   }
 }
@@ -88,11 +92,13 @@ inline U Matrix_to_dblint_matrix_(const Eigen::Matrix<T, Dynamic, Dynamic>& A) {
   return B;
 }
 
-inline doubles_matrix<> as_doubles_matrix(const Eigen::Matrix<double, Dynamic, Dynamic>& A) {
+inline doubles_matrix<> as_doubles_matrix(
+    const Eigen::Matrix<double, Dynamic, Dynamic>& A) {
   return Matrix_to_dblint_matrix_<double, doubles_matrix<>>(A);
 }
 
-inline integers_matrix<> as_integers_matrix(const Eigen::Matrix<int, Dynamic, Dynamic>& A) {
+inline integers_matrix<> as_integers_matrix(
+    const Eigen::Matrix<int, Dynamic, Dynamic>& A) {
   return Matrix_to_dblint_matrix_<int, integers_matrix<>>(A);
 }
 

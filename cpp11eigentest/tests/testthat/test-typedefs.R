d0746795b42ab3e0c2024256ed085f3642c8af01
Mauricio 'Pacha' Vargas Sepulveda <m.sepulveda@mail.utoronto.ca>
2024-09-09 12:45:06 -0400

    expand tests

--- cpp11eigentest/tests/testthat/test-typedefs.R
+++ cpp11eigentest/tests/testthat/test-typedefs.R
@@ -1,9 +1,36 @@
 test_that("elemental tests for vectors", {
   x <- c(1.0, 2.0, 3.0)
   expect_equal(x, typedef_Col_double(x))
+
+  x[1] <- NA
+  expect_equal(x, typedef_Col_double(x))
+
+  x[1] <- NaN
+  expect_equal(x, typedef_Col_double(x))
+
+  x[1] <- Inf
+  expect_equal(x, typedef_Col_double(x))
+
+  x[1] <- -Inf
+  expect_equal(x, typedef_Col_double(x))
   
   y <- c(1L, 2L, 3L)
   expect_equal(y, typedef_Col_int(y))
+
+  y[1] <- NA
+  expect_equal(y, typedef_Col_int(y))
+
+  y[1] <- NaN
+  expect_error(typedef_Col_int(y), "expected 'integer' actual 'double'")
+  expect_equal(y, typedef_Col_double(y))
+
+  y[1] <- Inf
+  expect_error(typedef_Col_int(y), "expected 'integer' actual 'double'")
+  expect_equal(y, typedef_Col_double(y))
+
+  y[1] <- -Inf
+  expect_error(typedef_Col_int(y), "expected 'integer' actual 'double'")
+  expect_equal(y, typedef_Col_double(y))
 })
 
 test_that("elemental tests for matrices", {
@@ -11,6 +38,33 @@ test_that("elemental tests for matrices", {
   x <- matrix(rnorm(4), nrow = 2)
   expect_equal(x, typedef_Mat_double(x))
 
+  x[1,1] <- NA
+  expect_equal(x, typedef_Mat_double(x))
+
+  x[1,1] <- NaN
+  expect_equal(x, typedef_Mat_double(x))
+
+  x[1,1] <- Inf
+  expect_equal(x, typedef_Mat_double(x))
+
+  x[1,1] <- -Inf
+  expect_equal(x, typedef_Mat_double(x))
+
   y <- matrix(rpois(4,1), nrow = 2)
   expect_equal(y, typedef_Mat_int(y))
+
+  y[1,1] <- NA
+  expect_equal(y, typedef_Mat_int(y))
+
+  y[1,1] <- NaN
+  expect_error(typedef_Mat_int(y), "expected 'integer' actual 'double'")
+  expect_equal(y, typedef_Mat_double(y))
+
+  y[1,1] <- Inf
+  expect_error(typedef_Mat_int(y), "expected 'integer' actual 'double'")
+  expect_equal(y, typedef_Mat_double(y))
+
+  y[1,1] <- -Inf
+  expect_error(typedef_Mat_int(y), "expected 'integer' actual 'double'")
+  expect_equal(y, typedef_Mat_double(y))
 })
